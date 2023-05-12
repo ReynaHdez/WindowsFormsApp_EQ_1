11b23f7dd09ef1c3ac6d587166a536005f01b26c
ReynaSantos501 <104239596+ReynaHdez@users.noreply.github.com>
2023-05-12 17:29:21 -0600

    clase 11 de mayo

--- WindowsFormsApp_EQ_1/Pc_2023_1_ComparaNumeros.cs
+++ WindowsFormsApp_EQ_1/Pc_2023_1_ComparaNumeros.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApp_EQ_1
     public partial class Pc_2023_1_ComparaNumeros : Form
     {
         [DllImport(@"C:\Users\Reyna\source\repos\PM_U5_EQ_1\Debug\PM_U5_EQ_1.dll")]
-        static extern int comparaNumeros(StringBuilder sb, string nombre, int numero1, int numero2, int totdigitos);
+        static extern IntPtr comparaNumeros(StringBuilder sb, string nombre, int[] auxVector1);
         public Pc_2023_1_ComparaNumeros()
         {
             InitializeComponent();
@@ -35,12 +35,44 @@ namespace WindowsFormsApp_EQ_1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            /*
             sb = new StringBuilder();
             sb.Clear();
             int largo = comparaNumeros(sb, "Picas y Fijas", 56, 59, 2);
             string c = sb.ToString();
             c = c.Substring(0, largo);
             MessageBox.Show(c);
+            */
+
+            sb = new StringBuilder(100);
+            sb.Clear();
+
+            int[] arregloNums = new int[6];
+            arregloNums[0] = 2; 
+            arregloNums[1] = 10;
+            arregloNums[2] = 2;
+            arregloNums[3] = 3;
+            arregloNums[4] = 7;
+            arregloNums[5] = 8;
+            
+            /*
+            for(int i = 0; i < arregloNums.Length; i++)
+            {
+                MessageBox.Show(arregloNums[i].ToString());
+            }
+            */
+
+            IntPtr ptr = comparaNumeros(sb, "Cadena ", arregloNums);
+            int arrayLength = Marshal.ReadInt32(ptr);
+
+            IntPtr start = IntPtr.Add(ptr, 4);
+            int[] result = new int[arrayLength];
+            Marshal.Copy(start, result, 0, arrayLength);
+
+            for(int i = 0; i < result.Length; i++)
+            {
+                MessageBox.Show(result[i].ToString());
+            }
         }
 
         private void txtDatos_TextChanged(object sender, EventArgs e)
